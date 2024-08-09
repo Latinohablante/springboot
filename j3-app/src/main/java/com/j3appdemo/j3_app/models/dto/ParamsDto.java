efae5b5a9839d09f4b19e52431fb2a2173a1cbf0
Latinohablante <131440605+Latinohablante@users.noreply.github.com>
2024-08-09 17:46:51 -0500

    Add files via upload

--- j3-app/src/main/java/com/j3appdemo/j3_app/models/dto/ParamsDto.java
+++ j3-app/src/main/java/com/j3appdemo/j3_app/models/dto/ParamsDto.java
@@ -2,9 +2,20 @@ package com.j3appdemo.j3_app.models.dto;
 
 public class ParamsDto {
     private String message;
+    private Integer code;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
     public String getMessage() {
         return message;
     }
+
     public void setMessage(String message) {
         this.message = message;
     }
