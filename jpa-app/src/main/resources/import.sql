531e2dc89c2728623d9752e4cd234c85aa19840e
Latinohablante <131440605+Latinohablante@users.noreply.github.com>
2024-08-13 17:08:20 -0500

    Add files via upload

---
+++ jpa-app/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Johlver','Pardo','Java')
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Carlos','Rueda','Python')
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Miguel','Mejia','JavaScript')
