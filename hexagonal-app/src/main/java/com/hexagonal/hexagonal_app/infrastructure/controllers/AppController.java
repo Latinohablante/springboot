e20b79116658702f0719e62856a1a5e42c3fb5d6
Latinohablante <131440605+Latinohablante@users.noreply.github.com>
2024-08-27 17:38:34 -0500

    manejo de excepciones

---
+++ hexagonal-app/src/main/java/com/hexagonal/hexagonal_app/infrastructure/controllers/AppController.java
@@ -0,0 +1,13 @@
+package com.hexagonal.hexagonal_app.infrastructure.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    @GetMapping("/app")
+    public String index() {
+        int valor = 100/0;
+        return "Ok 200";
+    }
+}
---
+++ hexagonal-app/src/main/java/com/hexagonal/hexagonal_app/infrastructure/exceptioncontrollers/HandlerExceptionController.java
@@ -0,0 +1,47 @@
+package com.hexagonal.hexagonal_app.infrastructure.exceptioncontrollers;
+
+
+
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import com.hexagonal.hexagonal_app.infrastructure.models.ErrorCustom;
+
+
+@RestControllerAdvice
+public class HandlerExceptionController {
+
+    @ExceptionHandler({ArithmeticException.class})
+
+    public ResponseEntity<ErrorCustom> divisionByZero(Exception ex) {
+
+        ErrorCustom error = new ErrorCustom();
+        error.setDate(new Date());
+        error.setError("Error división por cero!");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        //return ResponseEntity.internalServerError().body(error);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+
+    public ResponseEntity<ErrorCustom> notFoundEx(NoResourceFoundException e) {
+
+        ErrorCustom error = new ErrorCustom();
+        error.setDate(new Date());
+        error.setError("Api rest no encontrado");
+        error.setMessage(e.getMessage());
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+    }
+}
